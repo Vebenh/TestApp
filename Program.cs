eb59a41eb36b30c04118aecd12118766463493c6
Makar Konovalov <vebenh@gmail.com>
2023-08-02 21:51:17 +0200

    Another test commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main()
     {
         new Delegates(Console.ReadLine(), Console.ReadLine(), 10, 5, Delegates.Operations.Mult);
+        //Some comment
         Console.ReadLine();
 
     }
