3722fcc6e4bf72ba3d5920057a1ca5bc200b844a
Makar Konovalov <vebenh@gmail.com>
2023-08-06 00:29:35 +0200

    EntityFramework learning #2

--- Program.cs
+++ Program.cs
@@ -8,16 +8,27 @@ class Program
 {
     static void Main()
     {
-        using (var dbContext = new ApplicationContext())
+        using (ApplicationContext db = new ApplicationContext())
         {
-            try
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            // создаем два объекта User
+            User user1 = new User { Name = "Tom", Age = 33 };
+            User user2 = new User { Name = "Alice", Age = 26 };
+
+            // добавляем их в бд
+            db.Users.AddRange(user1, user2);
+            db.SaveChanges();
+        }
+        // получение данных
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            // получаем объекты из бд и выводим на консоль
+            var users = db.Users.ToList();
+            Console.WriteLine("Users list:");
+            foreach (User u in users)
             {
-                var serverVersion = dbContext.Database.ExecuteSqlRaw("SELECT @@VERSION").ToString();
-                Console.WriteLine($"Подключение успешно! Версия сервера: {serverVersion}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка подключения к базе данных: {ex.Message}");
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
             }
         }
 
