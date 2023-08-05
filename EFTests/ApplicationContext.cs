6b6706714d2254b6dd169c00bea1d61383585a1b
Makar Konovalov <vebenh@gmail.com>
2023-08-05 23:19:25 +0200

    EntityFramework learning #1

---
+++ EFTests/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp.EFTests;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = @"Server=127.0.0.1,2022;Database=TestDatabase;User=sa;Password=v2v3v5v8v9vfF;Encrypt=False";
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using TestApp.Delegates;
+﻿using Microsoft.EntityFrameworkCore;
+
+using TestApp.EFTests;
+using TestApp.Delegates;
+
 
 class Program
 {
     static void Main()
     {
-        new Delegates(Console.ReadLine(), Console.ReadLine(), 10, 5, Delegates.Operations.Mult);
-        //Some comment
-        Console.ReadLine();
+        using (var dbContext = new ApplicationContext())
+        {
+            try
+            {
+                var serverVersion = dbContext.Database.ExecuteSqlRaw("SELECT @@VERSION").ToString();
+                Console.WriteLine($"Подключение успешно! Версия сервера: {serverVersion}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения к базе данных: {ex.Message}");
+            }
+        }
 
+        Console.ReadLine();
     }
 }
